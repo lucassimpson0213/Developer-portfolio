5fb8b2ab84d072d35b4287c1815629d8f5b3cb85
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-01-30 01:23:57 +0000

    changed vite.config.ts file to have build output go to dist for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  }
-})
+// vite.config.js
+export default {
+  build: {
+    outDir: "dist", // Change this to your desired output directory
+  },
+};
