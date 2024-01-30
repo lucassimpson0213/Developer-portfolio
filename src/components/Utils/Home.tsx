3acbb99c0737f0e7f1931d897a37a1c029193ff6
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-01-30 02:00:40 +0000

    made changes for mobile including breakpoints for the Flex boxes that will turn it to a column after a certain screen size.

--- src/components/Utils/Home.tsx
+++ src/components/Utils/Home.tsx
@@ -1,5 +1,4 @@
-import { Box, Button, ButtonGroup } from "@chakra-ui/react";
-
+import { Button, ButtonGroup, Flex } from "@chakra-ui/react";
 import NewModel from "./NewModel";
 import { Link } from "react-router-dom";
 
@@ -7,7 +6,7 @@ export function Home() {
   // Define the button labels and their corresponding routes
   const buttons = [
     { label: "Home", path: "/" },
-    { label: "About", path: "/about" }, // Changed from '/contact' to '/about'
+    { label: "About", path: "/about" },
     { label: "Skills", path: "/skills" },
     { label: "Blog", path: "/blog" },
     { label: "SignUp", path: "/signup" },
@@ -15,32 +14,45 @@ export function Home() {
   ];
 
   return (
-    <Box className="bg-gray-600 rounded flex flex-row h-1/2 w-screen justify-center">
-      <div className="flex flex-row justify-center items-center">
-        <ButtonGroup>
-          {buttons.map(({ label, path }) => (
-            <Button
-              fontFamily={"adobe-garamond-pro"}
-              className="justify-center items-center m-14 size-30 text-xl text-white"
-              key={label}
-            >
-              <Link to={path}>{label}</Link>
-            </Button>
-          ))}
-          {/* If you have a contact route
-          <Button><Link to='/contact'>Contact</Link></Button> */}
-        </ButtonGroup>
+    <Flex
+      flexDir={{ base: "column", md: "row" }}
+      justify="center"
+      spacing={40}
+      align="center"
+      w={"100%"} // Stack columns on mobile and use row on medium and larger screens
+      h="auto" // Automatically adjust the height based on content
+      bg="gray.600"
+      rounded="3xl"
+      p={{ base: 3, md: 5 }} // Padding on mobile and larger screens
+    >
+      <Flex
+        justify="center"
+        direction="row"
+        w={{ base: "100%", md: "50%" }}
+        spacing={20}
+        align="center"
+        flexDir={{ base: "column", md: "row" }}
+      >
+        {buttons.map(({ label, path }) => (
+          <Button
+            fontFamily="adobe-garamond-pro"
+            className="justify-center items-center m-2 size-30 text-xl text-white"
+            key={label}
+          >
+            <Link to={path}>{label}</Link>
+          </Button>
+        ))}
+      </Flex>
 
-        <NewModel
-          ModelTitle="Introduction"
-          ModelText={`Hello this is my webpage! 
-                You can go down to the homepage for my introduction.
-                 There's also a skills section where I display
-                 some of the skills I've gained over my time here!
-                 I like to do web development, gaming and I also like to work out!`}
-        />
-      </div>
-    </Box>
+      <NewModel
+        ModelTitle="Introduction"
+        ModelText={`Hello, this is my webpage! 
+          You can go down to the homepage for my introduction.
+          There's also a skills section where I display
+          some of the skills I've gained over my time here!
+          I like to do web development, gaming, and I also like to work out!`}
+      />
+    </Flex>
   );
 }
 
