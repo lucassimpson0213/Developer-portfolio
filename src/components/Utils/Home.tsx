e02bc1cd94f00d8d5945e980240d520a701fcae8
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-01-30 02:11:23 +0000

    added mobile screen for navbar, now have to focus on components

--- src/components/Utils/Home.tsx
+++ src/components/Utils/Home.tsx
@@ -17,41 +17,40 @@ export function Home() {
     <Flex
       flexDir={{ base: "column", md: "row" }}
       justify="center"
-      spacing={40}
       align="center"
-      w={"100%"} // Stack columns on mobile and use row on medium and larger screens
+      w="100%" // Take up full width on all screen sizes
       h="auto" // Automatically adjust the height based on content
       bg="gray.600"
       rounded="3xl"
-      p={{ base: 3, md: 5 }} // Padding on mobile and larger screens
+      p={{ base: 1, md: 5 }} // Padding on mobile and larger screens
     >
       <Flex
         justify="center"
         direction="row"
-        w={{ base: "100%", md: "50%" }}
-        spacing={20}
+        h={"auto"}
+        w={"90vw"} // Take up 100% width on small screens, 50% on medium and larger screens
+        // Adjust spacing between buttons
         align="center"
         flexDir={{ base: "column", md: "row" }}
       >
         {buttons.map(({ label, path }) => (
           <Button
             fontFamily="adobe-garamond-pro"
-            className="justify-center items-center m-2 size-30 text-xl text-white"
+            className="size-30 text-xl text-white m-5"
             key={label}
           >
             <Link to={path}>{label}</Link>
           </Button>
         ))}
-      </Flex>
-
-      <NewModel
-        ModelTitle="Introduction"
-        ModelText={`Hello, this is my webpage! 
+        <NewModel
+          ModelTitle="Introduction"
+          ModelText={`Hello, this is my webpage! 
           You can go down to the homepage for my introduction.
           There's also a skills section where I display
           some of the skills I've gained over my time here!
           I like to do web development, gaming, and I also like to work out!`}
-      />
+        />
+      </Flex>
     </Flex>
   );
 }
