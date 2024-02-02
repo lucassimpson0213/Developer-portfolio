a909606985fbf321ee3ee66013793314e3fa0ad3
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-02-02 23:46:03 +0000

    removed framer from homepage

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -8,94 +8,82 @@ import { GiPointing } from "react-icons/gi";
 import { CiInstagram, CiFacebook } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
 function HomePage() {
-  const contentVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
-  };
-
   return (
     <>
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={contentVariants}
-        layout
-      >
-        <Flex w="100%" alignItems="center" justify="center">
-          <Box
-            alignContent={"center"}
-            textAlign="center"
-            marginTop="4rem"
-            width="30%"
-          >
-            <div>
-              <Heading
-                fontFamily={"adobe-garamond-pro"}
-                fontSize="3xl"
-                marginBottom="1rem"
-              >
-                <PiHandWaving size={60} />
-                Hi there, I'm Lucas!
-              </Heading>
-              <Text fontFamily={"adobe-garamond-pro"} mb={3}>
-                This is my webpage! I am a junior at Illinois state university!{" "}
-              </Text>
-              <Text fontFamily={"adobe-garamond-pro"}>
-                I study Computer Science and I have an internship with GrowMark
-                for Summer 2024
-              </Text>
-              <Text>
-                <div></div>
-              </Text>
-              <Text fontFamily={"adobe-garamond-pro"}>
-                I used React and several component Libraries to construct this.
-                I enjoy doing web development in my spare time.
-              </Text>
-              <Text fontFamily={"adobe-garamond-pro"}>
-                Check out this project on github. My socials are linked below
-              </Text>{" "}
-              <GiPointing size={40} />
-            </div>
-          </Box>
-          <Image
-            ml={10}
-            borderRadius="full"
-            boxSize="120px"
-            src={src1}
-            alt="My girlfriend krystal and I"
-          />
-        </Flex>
-
-        <Flex
-          mt={20}
-          w={"91vw"}
-          justify={"center"}
-          align={"center"}
-          direction={"row"}
+      <Flex w="100%" alignItems="center" justify="center">
+        <Box
+          alignContent={"center"}
+          textAlign="center"
+          marginTop="4rem"
+          width="30%"
         >
-          <a
-            href="https://www.instagram.com/lucassimpson33/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <CiInstagram className="mr-5" size={"40"} />
-          </a>
-          <a
-            href="https://www.facebook.com/profile.php?id=100005845240530"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <CiFacebook className="mr-5" size={"40"} />
-          </a>
-          <a
-            href="https://github.com/lucassimpson0213"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub className="mr-5" size={"40"} />
-          </a>
-        </Flex>
-      </motion.div>
+          <div>
+            <Heading
+              fontFamily={"adobe-garamond-pro"}
+              fontSize="3xl"
+              marginBottom="1rem"
+            >
+              <PiHandWaving size={60} />
+              Hi there, I'm Lucas!
+            </Heading>
+            <Text fontFamily={"adobe-garamond-pro"} mb={3}>
+              This is my webpage! I am a junior at Illinois state university!{" "}
+            </Text>
+            <Text fontFamily={"adobe-garamond-pro"}>
+              I study Computer Science and I have an internship with GrowMark
+              for Summer 2024
+            </Text>
+            <Text>
+              <div></div>
+            </Text>
+            <Text fontFamily={"adobe-garamond-pro"}>
+              I used React and several component Libraries to construct this. I
+              enjoy doing web development in my spare time.
+            </Text>
+            <Text fontFamily={"adobe-garamond-pro"}>
+              Check out this project on github. My socials are linked below
+            </Text>{" "}
+            <GiPointing size={40} />
+          </div>
+        </Box>
+        <Image
+          ml={10}
+          borderRadius="full"
+          boxSize="120px"
+          src={src1}
+          alt="My girlfriend krystal and I"
+        />
+      </Flex>
+
+      <Flex
+        mt={20}
+        w={"91vw"}
+        justify={"center"}
+        align={"center"}
+        direction={"row"}
+      >
+        <a
+          href="https://www.instagram.com/lucassimpson33/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <CiInstagram className="mr-5" size={"40"} />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100005845240530"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <CiFacebook className="mr-5" size={"40"} />
+        </a>
+        <a
+          href="https://github.com/lucassimpson0213"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="mr-5" size={"40"} />
+        </a>
+      </Flex>
     </>
   );
 }
