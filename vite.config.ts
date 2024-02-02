f1882caf59fc487052f3a4e2197058806bd75dde
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-02-02 23:10:44 +0000

    use vendor chunking with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from "path"; // Corrected import
 import react from "@vitejs/plugin-react";
+import { splitVendorChunkPlugin } from "vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Now 'path' is correctly imported
