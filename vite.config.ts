816d4bfb09ff0ffe37a23137ecc386db23717414
lucassimpson0213 <113938275+lucassimpson0213@users.noreply.github.com>
2024-02-03 00:28:17 +0000

    configured chunking for performance in rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import path from "path"; // Corrected import
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { splitVendorChunkPlugin } from "vite";
-import { defineConfig } from "vite";
+import path from "path";
+
+const manualChunks = function (id) {
+  if (id.includes("node_modules"))
+    return id.toString().split("node_modules/")[1].split("/")[0].toString();
+};
 
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: manualChunks,
+      },
+    },
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Now 'path' is correctly imported
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
